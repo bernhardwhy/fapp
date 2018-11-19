227243527b63b774fc4010a81bdfcdfe50671010
Berni Why <hello@wytek.info>
2018-11-19 21:41:50 +0100

    added firebase

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'
 import thunk from 'redux-thunk';
+import { combineReducers } from 'redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
-import reducer from './store/reducer/reducer';
+import baseReducer from './store/reducer/reducer';
+import { firebaseReducer } from 'react-redux-firebase'
+
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    baseReducer: baseReducer,
+  });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const startApp = () => {
     ReactDOM.render(<Provider store={ store }><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
